38e2d22112c27f6ff4b4f98e9cdbbbab55e12ef1
MohdElyas19 <mdiliyaskhan21@gmail.com>
2022-05-21 20:54:07 +0530

    checking update

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package com.elyas;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Name: Mohd Elyas Khan");
+		System.out.println("Father's Name: Md Usmangani");
+		System.out.println("Mother's Name: Khadija");
+		System.out.println("Age: 22");
+		System.out.println("Genger: Male");
+		System.out.println("Address: Manipur");
+		System.out.println("Mobile No: 0123456789");
+	}
+}
---
+++ Demo2.java
@@ -0,0 +1,7 @@
+package com.elyas;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome back");
+	}
+}
